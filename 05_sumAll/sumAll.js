693fef967aa6eb651f7b46859a7e437638379719
Robert Jones <dynasty_warrior201990@yahoo.com>
2022-06-24 08:50:34 -0500

    Finished Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) {
+        return "ERROR";
+    }
+    if (numberOne < 0 || numberTwo < 0 ){
+        return "ERROR";
+    }
+    if (numberOne > numberTwo) {
+        const tempNumber = numberOne;
+        numberOne = numberTwo;
+        numberTwo = tempNumber;
+    }
+let sum = 0;
+for (let i = numberOne; i <= numberTwo; i++){
+    sum += i;
+}
 
-};
+return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
