60721bbe3301df492f8faf9e80404874a2c1ff80
Robert Jones <dynasty_warrior201990@yahoo.com>
2022-06-24 08:48:07 -0500

    finished exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(wordBeingTested, numberToRepeat) {
+    if (numberToRepeat < 0) return 'ERROR';
+    let wordBeingRepeated = '';
+    for (let i = 0; i < numberToRepeat; i++) {
+      wordBeingRepeated += wordBeingTested;
+    }
+    return wordBeingRepeated;
+  };
+  
 
 // Do not edit below this line
 module.exports = repeatString;
