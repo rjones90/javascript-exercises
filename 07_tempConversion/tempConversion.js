84302190a1de1c4b1c679e89a2f6b54a899374ad
Robert Jones <dynasty_warrior201990@yahoo.com>
2022-06-24 08:44:16 -0500

    Finished Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  celsius = (temperature -32) * .5556;
+return celsius = parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  fahrenheit = (temperature * 1.8) + 32;
+return fahrenheit = parseFloat(fahrenheit.toFixed(1));
 };
+
 
 // Do not edit below this line
 module.exports = {
