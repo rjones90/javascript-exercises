ba5dee551d6971c6ee5eb798d6e494132b4ef4c3
Robert Jones <dynasty_warrior201990@yahoo.com>
2022-06-24 08:49:39 -0500

    Finished Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (...thingsToRemove) {
+    const array = thingsToRemove[0];
+    const newArray = [];
+    array.forEach((item) => {
+    if (!thingsToRemove.includes(item)){
+        newArray.push(item);
+    }
+    
+});
 
+return newArray;
 };
 
 // Do not edit below this line
